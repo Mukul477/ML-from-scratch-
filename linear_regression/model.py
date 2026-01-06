f1ff2a247916854269009e828ffe5c78831325a0
Mukul477 <muku123.singh2018@gmail.com>
2026-01-06 09:21:56 +0530

    fit: start fit method

--- linear_regression/model.py
+++ linear_regression/model.py
@@ -4,7 +4,7 @@ class LinearRegression:
         self.b = 0.0
 
     def fit(self, X, y, lr=0.01, epochs=100):
-        pass
+        n = len(X)
     def predict(self, X):
         preds = []
         for x in X:
